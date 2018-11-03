6ccb0ad2634c4c7021ee2df893d7fde3df121ecb
mopack <mopackp@gmail.com>
2018-11-03 23:44:59 +0800

    Upload

---
+++ GameRollABall/Assets/Script/PlayerController.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+	public float Speed;
+	private Rigidbody rb;
+	public AudioClip soundPickUp;
+
+	private int Count;
+	public Text CountText;
+	public Text WinText;
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody> ();
+
+		Count = 0;
+		SetCounterText ();
+		WinText.text = "";
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+		//rb.AddForce (transform.forward);
+		rb.AddForce(movement * Speed);
+	}
+
+	void OnTriggerEnter(Collider other){
+		if (other.gameObject.CompareTag ("PickUp")) {
+			Count = Count + 1;
+			SetCounterText ();
+			AudioSource.PlayClipAtPoint (soundPickUp, transform.position);
+			other.gameObject.SetActive (false);
+		}
+	}
+
+	void SetCounterText(){
+		CountText.text = "Count = " + Count.ToString ();
+		if (Count == 5) {
+			WinText.text = "You win!";
+		}
+	}
+}
---
+++ GameRollABall/README.md
@@ -0,0 +1,19 @@
+# UnityGame: RollABall
+
+RollABall is a 3d game written in C# and using Unity engine to render.
+
+Try to get all the targets on board to win the game.
+
+## Gameplay
+
+Watch RollABall Gameplay on Youtube.
+
+[![RollABall Gameplay](https://img.youtube.com/vi/j9EEmNEiDwI/0.jpg)](https://www.youtube.com/watch?v=j9EEmNEiDwI)
+
+## Information
+
+* C# Script:        UnityGameRollABall\Assets\Script
+
+* Release Build:    UnityGameRollABall\Builds\Roll A Ball (Release Build).7z
+
+* Sounds:           UnityGameRollABall\Assets\Sounds
